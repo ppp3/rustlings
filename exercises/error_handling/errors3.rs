786069c1ad8b843780d01b75e7fa1e25a79619c6
yourname <youremail@example.com>
2022-04-05 19:01:49 +0200

    Error3.rs gelöst

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -8,28 +8,31 @@
 
 use std::num::ParseIntError;
 
+
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    match total_cost(pretend_user_input){
+        Ok(cost) =>
+            {
+                if cost > tokens {
+                    println!("You can't afford that many!");
+                } else {
+                    tokens -= cost;
+                    println!("You now have {} tokens.", tokens);
+                }
 
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+            },
+        Err(i) =>println!("Schief gegangen"),
     }
+
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
-    match qty
-    {
-        Ok(i) => return Ok(i),
-        Err(i) => return Err(i)
-    }
-    //Ok(qty * cost_per_item + processing_fee)
+    let qty=item_quantity.parse::<i32>()?;
+    Ok(qty * cost_per_item + processing_fee)
 }
+
