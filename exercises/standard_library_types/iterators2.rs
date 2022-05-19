e4c7ab870073c4adb1a29a8f0dcb891074f08fce
yourname <youremail@example.com>
2022-05-19 14:34:45 +0200

    iterators2.rs gelöst

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -12,7 +11,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) =>{
+            let first_Uppercase =first.to_uppercase().to_string();
+            let rest=c.as_str().to_string();
+            format!("{}{}",first_Uppercase,rest)
+        } 
     }
 }
 
@@ -21,15 +24,24 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+        let mut result:Vec<String> =vec![];
+        for x in words
+        {
+            result.push(capitalize_first(x));
+        }
+        result
 }
-
 // Step 3.
 // Apply the `capitalize_first` function again to a slice of string slices.
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut result=String::new();
+    for word in words
+    {
+        result=format!("{}{}", result, capitalize_first(word))
+    }
+    result
 }
 
 #[cfg(test)]
