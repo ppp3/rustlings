4db6aa3623a40cf12902fa28834c907c178e1db2
yourname <youremail@example.com>
2022-05-19 16:26:08 +0200

    iterators3.rs gelöst

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -6,7 +6,6 @@
 //    list_of_results functions.
 // Execute `rustlings hint iterators3` to get some hints!
 
-// I AM NOT DONE
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
@@ -38,16 +37,36 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+fn result_with_list() -> Result<Vec<i32>, DivisionError>
+{
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
-}
+    let division_results= numbers.into_iter().map(|n| divide(n, 27));
+    let mut v: Vec<i32>=vec![];
+    for element in division_results
+    {
+    	match element
+	{
+	    Ok(a) => v.push(a),
+            _     =>{}
 
+	}
+
+    }
+    Ok(v)
+
+    //Ok([1, 2, 3, 4])
+}
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let mut v:Vec<Result<i32, DivisionError>>=vec![]; 
+    for element in division_results
+    {
+	v.push(element);
+    }	    
+    v
 }
 
 #[cfg(test)]
